f6e439d75c0d9aa312d657d8cd0f9e713a979fcb
Infatum <qwertty459@gmail.com>
2017-03-04 01:20:15 +0200

    translator part^ smallfix

--- translator.h
+++ translator.h
@@ -1,4 +1,23 @@
 #ifndef TRANSLATOR_H
 #define TRANSLATOR_H
+#include "translator.h"
+#include "parser.h"
+
+void start_translation();
+
+template <typename T>
+T translate_function();
+
+template <typename T>
+bool translate_relational_operation(T lval, T rval);
+
+template <typename T>
+void translate_loop(int start, int end, T &obj);
+
+template <typename T>
+T assign_operation(T &lval, const T &rval);
+
+template <typename T>
+T translate_statement(int operator_type, T &lval, vector<T> rvals);
 
 #endif // TRANSLATOR_H
